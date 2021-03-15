8718020e4427f1ad31f7db92401381cfa301f96f
DESKTOP-NUOPUIF\ITPS <hyunsybm@daum.net>
2021-03-15 17:19:34 +0900

    최초 프로젝트 업로드

---
+++ src/chapter1/Ex1.java
@@ -0,0 +1,23 @@
+package chapter1;
+
+//클래스:  소스 코드의 최소 단위 
+public class Ex1 {
+	//메인 함수: 시작점, 내가 작성한 소스코드의 시작이 어딘지 알려주는 기능
+	public static void main(String[] args) {
+		//주석: 메모, 개발자가 참고해야할 사항
+		//단축키 : ctrl+d 한 줄 지우기
+
+		
+		//아래 코드는 무언가를 출력하기 위한 기능
+		//()안에다가 출력 대상을 지정
+		System.out.println("Hello World");	//	
+		System.out.println("주석 연습중..");	//
+		System.out.println("출력1");
+		System.out.println("출력2");
+		// ctrl+shift+c 를 누르면 해당 코드 주석처리됨, 다시 그대로 누르면 주석처리 사라짐
+		// 여러줄 하고 싶으면 여러줄을 드래그해서 위 단축키를 누름
+		
+		
+	}
+
+}
---
+++ src/chapter1/Ex2.java
@@ -0,0 +1,42 @@
+package chapter1;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		//문자열-문자들의 나열
+		//문자열은 꼭 ""를 사용해서 표현해야함
+//		System.out.println("Hello World");
+//		System.out.println("Hello");
+//		System.out.println("World");
+
+//		예제1
+//		System.out.print("I am ");
+//		System.out.print("a ");
+//		System.out.print("Java Programer");
+		
+//		예제2
+//		System.out.print("오늘은 ");
+//		System.out.print("자바수업 ");
+//		System.out.println("첫째날입니다");
+		
+//		예제3
+		//사용하는 데이터의 형태1 정수(양수,0,음수)
+		System.out.println(1);
+		
+		//실수(양수,음수)
+		System.out.println(3.14);
+		
+		//문자(a-z,A-Z 등)
+		System.out.println('a');
+		
+		//문자열(abcd,안녕하세요)
+		System.out.println("Hello World");
+		
+//		예제4
+		System.out.println("오늘의 기온:"+(-5.3));
+		System.out.println("커피가격:"+1500);
+		System.out.println("철수의 Java 과목 학점:"+'A');
+		
+	}
+
+}
