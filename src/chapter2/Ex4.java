b62fe2b1af8ebc23b33e20a159d4facdf7771e97
DESKTOP-NUOPUIF\ITPS <hyunsybm@daum.net>
2021-03-17 17:49:29 +0900

    3일차추가

---
+++ src/chapter2/Ex4.java
@@ -0,0 +1,21 @@
+package chapter2;
+
+public class Ex4 {
+
+	public static void main(String[] args) {
+		/*
+		 * 가계부 작성 프로그램을 개발하려고 한다
+		 */
+		String[] date= {"3월2일","3월3일","3월4일","3월5일","3월6일"};
+		date[0]="3월 1일";
+		
+		int[] money= {32159,25157,87982,40839,18908};
+		char[] grade= {'B','A','D','B','A'};
+		
+		String[] month= {"3월","4월","5월"};
+		double[] avgMoney= {40315.67,111991.39,26433.7};
+		System.out.println(avgMoney[0]);
+
+	}
+
+}
