c726d70337250402c01c300ef4db7531a98c7b5a
DESKTOP-NUOPUIF\ITPS <hyunsybm@daum.net>
2021-03-30 14:03:14 +0900

    복습

--- src/chapter13/ParserTest.java
+++ src/chapter13/ParserTest.java
@@ -6,6 +6,7 @@ public class ParserTest {
 		Parseable parser=ParserManager.getParser("korean");
 		parser.parse("한글파일이 들어있는 파일.pdf");
 		
+		
 		parser=ParserManager.getParser("english");
 		parser.parse("영어파일이 들어있는 파일.pdf");
 	}
